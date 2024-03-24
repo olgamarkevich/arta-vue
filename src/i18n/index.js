7c571c107f87902c60a75b3450b9e8bff2e9f32f
Olga Markevich <pyrkoolga@gmail.com>
2024-03-24 16:00:44 +0300

    feat: language change in url

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -7,17 +7,66 @@ import fr from './locales/fr.json';
 import ja from './locales/ja.json';
 import pt from './locales/pt.json';
 
-const i18n = createI18n({
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages: {
+export const initI18n = () => {
+  /* Supported locales */
+  const defaultLocaleTag = 'en';
+
+  const locales = {
     de,
     en,
     es,
     fr,
     ja,
     pt,
-  },
-});
+  };
 
-export default i18n;
+  const localeTags = Object.keys(locales);
+
+  let localeTag = defaultLocaleTag;
+
+  /* URL param locale */
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const urlLocaleTag = urlParams.get('lang')?.toLowerCase();
+
+  if (
+    urlLocaleTag &&
+    localeTags.includes(urlLocaleTag) &&
+    urlLocaleTag !== defaultLocaleTag
+  ) {
+    localeTag = urlLocaleTag;
+  }
+
+  /* Browser locale tag */
+  if (!urlLocaleTag) {
+    const browserLocaleTags = navigator?.languages?.length
+      ? navigator.languages
+      : [...navigator?.language];
+
+    const splittedLocaleTags = browserLocaleTags.map(
+      (tag) => tag.split('-')[0]
+    );
+
+    let browserLocaleTag = null;
+
+    splittedLocaleTags.forEach((lang) => {
+      if (browserLocaleTag === null) {
+        if (localeTags.includes(lang)) {
+          browserLocaleTag = lang;
+        }
+      }
+    });
+
+    if (browserLocaleTag !== null && browserLocaleTag !== defaultLocaleTag) {
+      localeTag = browserLocaleTag;
+    }
+  }
+
+  const i18n = createI18n({
+    locale: localeTag,
+    fallbackLocale: defaultLocaleTag,
+    messages: locales,
+  });
+
+  return i18n;
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import i18n from './i18n';
+import { initI18n } from './i18n';
 
 const app = createApp(App);
+
+const i18n = initI18n();
 
 app.use(i18n);
 app.mount('#app');
